b6f9c0f4daf3dd1c671ff8d72f8ab7a489e79456
sai <saiaakash60@gmail.com>
2022-04-19 19:21:49 +0530

    Trainin script for DDPG

---
+++ scripts/training/ddpg_train.py
@@ -0,0 +1,36 @@
+import gym
+import rospy
+import numpy as np
+import os
+from openai_ros.openai_ros_common import StartOpenAI_ROS_Environment
+
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
+from stable_baselines import DDPG
+
+task_and_robot_environment_name = rospy.get_param(
+        '/drone/task_and_robot_environment_name')
+env = StartOpenAI_ROS_Environment(
+        task_and_robot_environment_name)
+#env = gym.make('MountainCarContinuous-v0')
+
+# the noise objects for DDPG
+n_actions = env.action_space.shape[-1]
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+logs_base_dir = './tensor_logs'
+os.makedirs(logs_base_dir, exist_ok=True)
+model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise, tensorboard_log=logs_base_dir)
+model.learn(total_timesteps=400000)
+model.save("ddpg_drone")
+
+#del model # remove to demonstrate saving and loading
+
+model = DDPG.load("ddpg_drone")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
